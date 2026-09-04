52ba1e209d285dd06b54e2591e244703c109b809
CortoGyt <corto.gayetun@gmail.com>
2026-09-04 15:05:46 +0200

    Ajout d'un HF_LABELLING_MODEL_ID-> model pour labelisé la liste intiale provenant de speedrun.com

--- config/settings.py
+++ config/settings.py
@@ -11,15 +11,16 @@ class Settings(BaseSettings):
     IGDB_ACCESS_TOKEN: str
     STEAM_API_KEY: str
 
-    # LLM - HuggingFace
-    HF_MODEL_ID: str = "meta-llama/Llama-2-7b-chat-hf"  # ou autre
+    # LLM  HuggingFace
+    HF_LABELLING_MODEL_ID: str = "Qwen/Qwen3-4B-Instruct-2507" #labellise le jeux à l'initial après récup via api speedrun.com
+    HF_MODEL_ID: str = "meta-llama/Llama-2-7b-chat-hf"  # placeholder, à check plus tard
     HF_TOKEN: str | None = None  # Token HuggingFace si modèle gated
 
     # SVD
     SVD_MODEL_PATH: str = "ml/svd/models/svd_model.pkl"
     SVD_DIMENSIONS: int = 50
 
-    # Vector DB - Chroma
+    # Vector DB Chroma
     CHROMA_DB_PATH: str = "ml/vectorstore/chroma_db"
     CHROMA_COLLECTION: str = "games"
 
